c0505b672b1292bcc6c10bd6a43de50ec7b415c6
Gourav Sachdeva <gouravsachdeva95@gmail.com>
2018-10-29 15:35:07 +0530

    Uphmis latest build

--- scripts/display-mode-controller.js
+++
@@ -1,9 +0,0 @@
-//Controller for column show/hide
-var trackerCapture = angular.module('trackerCapture');
-trackerCapture.controller('DisplayModeController',
-        function($scope, $modalInstance) {
-    
-    $scope.close = function () {
-      $modalInstance.close($scope.gridColumns);
-    };
-});
